4d26f2980f3162620f8d963b69f4997d2143acee
xukkz <wiggo.bergman@gmail.com>
2020-08-30 22:16:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ascii
+{
+    class Program
+    {
+        public static string input;
+        public static int i1;
+        public static char[,] array1 = new char[8, 15];
+        static void Main()
+        {
+            // i1 = Convert.ToInt16(Console.ReadLine());
+
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "0000":
+                    Console.WriteLine(input);
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    Main();
+                    break;
+            }
+        }
+    }
+}
