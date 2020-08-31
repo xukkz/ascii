2a25d956cf460db2b3b377cfd531d9054ee4670a
xukkz <60608121+xukkz@users.noreply.github.com>
2020-08-31 15:31:38 +0200

    Created binary converter

--- Program.cs
+++ Program.cs
@@ -4,26 +4,45 @@ namespace ascii
 {
     class Program
     {
+        public static string test;
         public static string input;
-        public static int i1;
-        public static char[,] array1 = new char[8, 15];
+        public static string result;
+        public static string calculateBinary(string x)
+        {
+            test = x;
+            result = Convert.ToString(Convert.ToInt32(test, 2), 10);
+
+            return result;
+        }
+
+
+        public static string[,] array2Db = new string[16, 2]
+        {
+            { "one", "two" },
+            { "three", "four" },
+            { "five", "six" },
+            {"seven", "eight"},
+            {"nine", "ten"},
+            {"eleven", "twelve"},
+            {"thirteen", "fourteen"},
+            {"fiftheen", "sixteen"},
+            {"seventeen", "eighteen"},
+            {"nineteen", "twenty"},
+            {"twnetyone", "twentytwo"},
+            {"twentithreee", "twentifouur"},
+            {"tentifife", "twentisex"},
+            {"twentiseven", "twentinine"},
+            {"twentiten", "twnetieleven"},
+            {"twentitweleve", "twentithireetn"}
+        };
+
         static void Main()
         {
-            // i1 = Convert.ToInt16(Console.ReadLine());
-
             input = Console.ReadLine();
 
-            switch (input)
-            {
-                case "0000":
-                    Console.WriteLine(input);
-                    Console.ReadLine();
-                    break;
-
-                default:
-                    Main();
-                    break;
-            }
+            Console.WriteLine(calculateBinary(input));
+            Console.ReadLine();
+            Main();
         }
     }
 }
